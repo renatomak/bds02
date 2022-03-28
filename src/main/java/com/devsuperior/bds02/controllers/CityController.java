515ae16daa55690eb5928bb79869a86b92c92aa4
renatomak <renato.mark.silva@gmail.com>
2022-03-28 08:33:22 -0300

    test: city controller method findAll

---
+++ src/main/java/com/devsuperior/bds02/controllers/CityController.java
@@ -0,0 +1,24 @@
+package com.devsuperior.bds02.controllers;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.services.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cities")
+public class CityController {
+
+    @Autowired
+    private CityService cityService;
+
+    @GetMapping
+    public ResponseEntity<List<CityDTO>> findAll() {
+        return ResponseEntity.ok(cityService.findAll());
+    }
+}
